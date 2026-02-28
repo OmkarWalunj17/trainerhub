92da6b7be1b49a6166484817ba6d4996910253a1
Omkar Walunj <omkarwalunj03@gmail.com>
2026-03-01 03:35:05 +0530

    Initial Commit

---
+++ trainerhub/src/test/java/com/omkar/trainerhub/TrainerhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omkar.trainerhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainerhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
